3fa672be22bc6a36939bad77e661610048e1395e
RockyChing <zdh1021680772@163.com>
2020-12-02 09:36:40 +0800

    Fix va_end missing bug in _sys_debug()

--- stm32f407/stm32f407_demo/user/utils/log_util.c
+++ stm32f407/stm32f407_demo/user/utils/log_util.c
@@ -22,7 +22,7 @@ static int g_log_level = LOG_DEBUG;
 
 int _sys_debug(int level, const char *tag, const char *fmt, ...)
 {
-	char buf[BUF_SIZE] = { 0 };
+	char buff[BUF_SIZE];
 	va_list ap;
 
 	if (level > g_log_level || level < 0) {
@@ -30,14 +30,16 @@ int _sys_debug(int level, const char *tag, const char *fmt, ...)
 	}
 
 	va_start(ap, fmt);
-	vsnprintf(buf, sizeof(buf) - 1, fmt, ap);
-	if (strstr(buf, "%s") != NULL) {
+	memset(buff, 0, sizeof(buff));
+	vsnprintf(buff, sizeof(buff) - 1, fmt, ap);
+	if (strstr(buff, "%s") != NULL) {
 		printf("WARN: log_debug called with '%%s' formatted string!");
 		va_end(ap);
 		return -1;
 	}
 
-	printf("%c/%s %s\n", g_log_level_char[level], tag, buf);
+	printf("%c/%s %s\n", g_log_level_char[level], tag, buff);
+	va_end(ap);
 	return 0;
 }
 
